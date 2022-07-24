ba6daf755b9978c5024ab97bb8af88e5165ec27f
Francisco Macuacua <macuacuafrancisco@gmail.com>
2022-07-24 11:48:56 +0200

    ALign Tutorials sequence, based on documentation

--- tutorial1/tutorial1/asgi.py
+++ tutorial1/tutorial1/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for tutorial3 project.
+ASGI config for tutorial1 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tutorial3.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tutorial1.settings')
 
 application = get_asgi_application()
--- tutorial1/tutorial1/urls.py
+++ tutorial1/tutorial1/urls.py
@@ -1,4 +1,4 @@
-"""tutorial3 URL Configuration
+"""tutorial1 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
